32911cce023e01903aef1947940847f24a272f66
rose-w <weiyanfengv@gmail.com>
2018-08-21 22:41:06 +0800

    修改src/encoder文件夹下内容

--- src/encoder/encoder_test.py
+++ src/encoder/encoder_test.py
@@ -1,1 +1,1 @@
-#hardware platform: pyboard V1.1from pyb import Pinimport timeimport encoderpin_x = Pin('X1',Pin.IN)pin_y = Pin('X2',Pin.IN)enc = encoder.Encoder(pin_x,pin_y,1,1)print('test start')data = 0while 1:    if data != enc.position:    print(enc.position)    data = enc.position  
+from machine import Pinimport timeimport encoderpin_x = Pin('X1',Pin.IN)pin_y = Pin('X2',Pin.IN)enc = encoder.Encoder(pin_x,pin_y,1,1)print('test start')data = 0while 1:    if data != enc.position:    print(enc.position)    data = enc.position  
