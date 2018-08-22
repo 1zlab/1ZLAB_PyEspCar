1e79fc2d9b4887afca3faf0c59278b16df98c310
ShunKai Xing <xingshunkai@qq.com>
2018-08-22 12:52:34 +0800

    完成电机角度PID控制

--- src/pid_motor.py
+++ src/pid_motor.py
@@ -1,3 +1,9 @@
+'''
+PID 
+P 比例 
+I 积分（惯性力）
+D 微分 (阻尼力)
+'''
 from machine import Timer
 from pid import PID
 
@@ -7,39 +13,19 @@ class MotorAngleControl(object):
     使用PID，结合编码器提供的反馈
     每隔100ms采样一次
     '''
-    def __init__(self, motor, encoder, kp, ki=0, kd=0, is_debug=False, scalar=4.5):
+    def __init__(self, motor, encoder, kp, ki=0, kd=0, is_debug=False):
         # 电机对象
         self.motor = motor
         # 编码器
         self.encoder = encoder
-        self.encoder.position = 0
-        # 角度与编码器计数之间的缩放因子
-        self.scalar = scalar
+        # 初始化编码器的计数
+        self.encoder._pos = 0
         # PID对象
         self.pid = PID(kp, ki, kd)
-        # 创建定时器 
-        # TODO 这里用的是定时器4
-        self.timer = Timer(4)
-        # 设置定时器回调 100ms执行一次
-        # TODO 测试10ms更新一次
-        self.timer.init(period=1, mode=Timer.PERIODIC, callback=self.callback)
-        # 是否重置计数器
-        self.is_reset = False
         # 是否开启调试模式
         self.is_debug = is_debug
-
-    def count2angle(self, count):
-        '''
-        将编码器计数转换为角度
-        '''
-        return count * self.scalar
-
-    def angle2count(self, angle):
-        '''
-        将角度转换为编码器计数
-        '''
-        return angle / self.scalar
-
+        self.count = 0
+        
     def set_angle(self, angle, is_reset=False):
         '''
         设置电机的旋转角度
@@ -49,17 +35,16 @@ class MotorAngleControl(object):
                 0度在编码器最初始的位置 
             * True: 如果连续旋转，旋转到特定角度的话，
                 编码器计数清零， 目标角度设定为0度,同时is_reset=False
+        BUG: is_reset 相反？
         '''
-        self.is_reset = is_reset
         # 设定目标计数
-        target_count = self.angle2count(angle)
+        target_count = angle
         if is_reset:
             # 计数器清零
-            self.encoder.count = 0
-        else:
-            # 目标计数器加上之前编码器的值
-            target_count += self.encoder.count
+            self.encoder._pos = 0
         
+        # 目标计数器加上之前编码器的值
+        target_count += self.encoder.position
         # 设置PID的目标取值
         self.pid.set_target_value(target_count)
 
@@ -68,20 +53,23 @@ class MotorAngleControl(object):
         回调函数
         '''
         # 获取当前编码器的真实取值
-        real_value = self.encoder.count
+        real_value = self.encoder.position   
         # 更新PID
-        result = self.pid.update(real_value)
-        
-        # 将result转换为电机转速
-        pwm = 1.0*result
+        pwm = self.pid.update(real_value)
+        if abs(real_value - self.pid.target_value) < 1:
+            pwm = 0
         # pwm的值放缩在 正负250-1023之间
-        # TODO ? pwm 也可以是0啊
-        self.motor.set_pwm(int(pwm))
-        if self.is_debug:
-            print("Target: {} RealValue: {} PID Result: {}".format(self.pid.target_value, self.encoder.count, result))
-            print('PWM: {}'.format(pwm))
-
+        # 为了准确，这里需要限定一下速度
+        if abs(pwm) > 350:
+            pwm = 350 if pwm > 0 else -350
         
+        self.motor.set_pwm(int(pwm))
+        self.count += 1
+        if self.is_debug and self.count > 10:
+            # 1s 打印一次
+            self.count = 0
+            print("Target: {} RealValue: {} PID Result: {}".format(self.pid.target_value, self.encoder.position, pwm))
+            print('PWM: {}'.format(pwm))
         
 class MotorSpeedPID(object):
     '''
--- src/test_pid_motor.py
+++ src/test_pid_motor.py
@@ -1,3 +1,4 @@
+from machine import Timer,Pin
 from pid_motor import MotorAngleControl
 from motor import Motor
 from encoder import Encoder
@@ -5,17 +6,30 @@ from car_config import gpio_dict
 
 
 # 左侧电机
-lmotor = Motor(gpio_dict['LEFT_MOTOR_A'], gpio_dict['LEFT_MOTOR_B']) #,motor_install_dir=False)
-lmotor.stop()
+left_motor = Motor(gpio_dict['LEFT_MOTOR_A'], gpio_dict['LEFT_MOTOR_B']) #,motor_install_dir=False)
+left_motor.stop()
 
 # 右侧电机
-rmotor = Motor(gpio_dict['RIGHT_MOTOR_A'], gpio_dict['RIGHT_MOTOR_B'])
-rmotor.stop()
+right_motor = Motor(gpio_dict['RIGHT_MOTOR_A'], gpio_dict['RIGHT_MOTOR_B'])
+right_motor.stop()
 
-# 左侧编码器
-lencoder = Encoder(gpio_dict['LEFT_ENCODER_A'], gpio_dict['LEFT_ENCODER_B'], 
-        3, motor_install_dir=False,name='LeftEncoder', is_debug=True)
+left_pin_a = Pin(gpio_dict['LEFT_ENCODER_A'], Pin.IN)
+left_pin_b = Pin(gpio_dict['LEFT_ENCODER_B'], Pin.IN)
+left_encoder = Encoder(left_pin_a, left_pin_b, reverse=1, scale=0.247)
 
-mac = MotorAngleControl(lmotor, lencoder, kp=-250, ki=0, kd=0, is_debug=False, scalar=4.5)
+right_pin_a = Pin(gpio_dict['RIGHT_ENCODER_A'], Pin.IN)
+right_pin_b = Pin(gpio_dict['RIGHT_ENCODER_B'], Pin.IN)
+right_encoder = Encoder(right_pin_a, right_pin_b, reverse=0, scale=0.247)
+
+# 15
+mac = MotorAngleControl(left_motor, left_encoder, kp=-9, ki=-0.3, kd=-4, is_debug=False)
+
+# 创建定时器 
+# 这里用的是定时器4
+timer = Timer(4)
+# 设置定时器回调 100ms执行一次
+# TODO 编码器是不是也会干扰到编码器计数
+timer.init(period=100, mode=Timer.PERIODIC, callback=mac.callback)
+
 # 90度 差不多是20个脉冲（不稳）
 mac.set_angle(90, is_reset=False)
