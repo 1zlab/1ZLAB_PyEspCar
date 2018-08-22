ee76727fbd1876f249a810b6ebd6dfd02ed49468
ShunKai Xing <xingshunkai@qq.com>
2018-08-22 14:56:31 +0800

    添加右侧电机的PID角度控制，添加PID参数到 car_config

--- src/test_pid_motor.py
+++ src/test_pid_motor.py
@@ -1,35 +1,58 @@
+'''
+测试PID控制电机旋转角度
+
+TODO 可视化调参
+滑动条 调节 ： KP KI KD，串口 + OpenCV
+波形绘制 （串口工具） or PyQT 开发上位机
+TODO 录制PID的课程
+TODO PID自动调参，
+设定标准，自动优化（+机器学习）
+'''
 from machine import Timer,Pin
 from pid_motor import MotorAngleControl
 from motor import Motor
 from encoder import Encoder
-from car_config import gpio_dict
-
+from car_config import gpio_dict, pid_param_dict
 
 # 左侧电机
 left_motor = Motor(gpio_dict['LEFT_MOTOR_A'], gpio_dict['LEFT_MOTOR_B']) #,motor_install_dir=False)
 left_motor.stop()
-
 # 右侧电机
 right_motor = Motor(gpio_dict['RIGHT_MOTOR_A'], gpio_dict['RIGHT_MOTOR_B'])
 right_motor.stop()
 
+# 左侧编码器管脚
 left_pin_a = Pin(gpio_dict['LEFT_ENCODER_A'], Pin.IN)
 left_pin_b = Pin(gpio_dict['LEFT_ENCODER_B'], Pin.IN)
+# 左侧编码器
 left_encoder = Encoder(left_pin_a, left_pin_b, reverse=1, scale=0.247)
 
+# 右侧编码器管脚
 right_pin_a = Pin(gpio_dict['RIGHT_ENCODER_A'], Pin.IN)
 right_pin_b = Pin(gpio_dict['RIGHT_ENCODER_B'], Pin.IN)
+# 右侧编码器
 right_encoder = Encoder(right_pin_a, right_pin_b, reverse=0, scale=0.247)
 
-# 15
-mac = MotorAngleControl(left_motor, left_encoder, kp=-9, ki=-0.3, kd=-4, is_debug=False)
+# PID参数
+kp = pid_param_dict['MOTOR_ANGLE_CTL_KP']
+ki = pid_param_dict['MOTOR_ANGLE_CTL_KI']
+kd = pid_param_dict['MOTOR_ANGLE_CTL_KD']
 
-# 创建定时器 
-# 这里用的是定时器4
+# 电机角度PID控制
+left_mac = MotorAngleControl(left_motor, left_encoder,
+		kp=kp, ki=ki, kd=kd, is_debug=False)
+right_mac = MotorAngleControl(right_motor, right_encoder,
+		kp=kp, ki=ki, kd=kd, is_debug=False)
+
+def pid_callback(timer):
+	left_mac.callback(timer)
+	right_mac.callback(timer)
+
+# 创建定时器 这里用的是定时器4
 timer = Timer(4)
 # 设置定时器回调 100ms执行一次
-# TODO 编码器是不是也会干扰到编码器计数
-timer.init(period=100, mode=Timer.PERIODIC, callback=mac.callback)
+timer.init(period=100, mode=Timer.PERIODIC, callback=pid_callback)
 
-# 90度 差不多是20个脉冲（不稳）
-mac.set_angle(90, is_reset=False)
+# 左侧电机正向旋转90度
+left_mac.set_angle(90, is_reset=False)
+right_mac.set_angle(90, is_reset=False)
