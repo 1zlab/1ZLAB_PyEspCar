05ff45827f8b529709b5687aea5cd0204bc2528a
ShunKai Xing <xingshunkai@qq.com>
2018-08-22 11:19:31 +0800

    添加编码器测试的注释

--- src/test_encoder.py
+++ src/test_encoder.py
@@ -1,8 +1,9 @@
 '''
 编码器测试
-
-电机旋转一周的精度在 1度以内
+-------------------------
 注意： print频次不能高，会影响计数
+电机旋转一周的计数是1480， 折算成360度角度，对应的scale取0.247
+电机旋转一周的精度在 1度以内
 '''
 from machine import Pin
 import time
@@ -24,9 +25,9 @@ right_counter = 0
 
 while True:
   if abs(left_counter-left_encoder.position) > 10:
-    print('LEFT: {}'.format(left_encoder.position))
+    print('Left Angle: {}'.format(left_encoder.position))
     left_counter = left_encoder.position
   
   if abs(right_counter-right_encoder.position) > 10:
-    print('RIGHT: {}'.format(right_encoder.position))
+    print('Right Angle: {}'.format(right_encoder.position))
     right_counter = right_encoder.position
