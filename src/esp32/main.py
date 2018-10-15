41462ad17bbaaad00b512a42590e3ccfb3078688
ShunKai Xing <xingshunkai@qq.com>
2018-10-15 20:57:30 +0800

    update main.py

--- src/esp32/main.py
+++ src/esp32/main.py
@@ -1,11 +1,2 @@
-'''
-程序入口
-'''
-from machine import Pin
-import utime
-
 if __name__ == '__main__':
-    # MQTT小车控制模式
-    # mqtt会影响WebREPL的连接，默认不运行
     exec(open('mqtt_control_mode.py').read(), globals())
-    # print('ESP32 Run Nothing')
